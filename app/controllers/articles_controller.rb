7ddef21637e69bc1235ebfc4546a348ca104423b
Chirag Bhargava <chirag.peace02@gmail.com>
2018-05-18 00:36:02 +0530

    Adding Read, Update and Index to the app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,45 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    render :index
+    if @article.save
+      flash[:notice] = "Article has been created successfully"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
 
   def index
+    @all_articles = Article.all
+  end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article has been updated successfully"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @article = Article.find(params[:id])
+    @article.destroy
+    if @article.exist?
+      render :index
+    else
+      flash[:notice] = "Article has been deleted successfully"
+      redirect_to articles_path(@article)
+    end
   end
 
   private
