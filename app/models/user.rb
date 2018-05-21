bec23e6c83374ed1354151a946715e8d14000a67
Chirag Bhargava <chirag.peace02@gmail.com>
2018-05-21 23:44:27 +0530

    Removing spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  validates :user_name, presence: true, 
+  validates :user_name, presence: true,
             uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :user_email, presence: true, 
-            uniqueness: { case_sensitive: false }, 
+  validates :user_email, presence: true,
+            uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
 end
